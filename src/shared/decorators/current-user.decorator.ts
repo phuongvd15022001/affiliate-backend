9f6b568f069db7d4205462fde05539c8fde49d78
Dipro <phuongvd@dipro.vn>
2026-06-04 15:25:08 +0700

    feat: fix eslint

--- src/shared/decorators/current-user.decorator.ts
+++ src/shared/decorators/current-user.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
+    const request = ctx.switchToHttp().getRequest<{ user: unknown }>();
     return request.user;
   },
 );
